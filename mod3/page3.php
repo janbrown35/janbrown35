be6ea50d32b9e38c72fe7a383482860ead55d705
janbrown35 <43588975+janbrown35@users.noreply.github.com>
2019-04-28 10:33:16 -0500

    Add files via upload

--- mod3/page3.php
+++ mod3/page3.php
@@ -1,41 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
   <head>
+    <title>Radiology Support Unite Home Page</title>
     <meta charset=utf-8>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
-<script src="js/scripts.js"></script>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="style3.css" rel="stylesheet" type="text/css">
+    <link href="style-3dh.css" rel="stylesheet" type="text/css">
+    <style type="text/css">
 
-  <title> Radiology Support Unite Events and Education</title>
-
-
+figure {float:left; margin:15 20px 0 0;}
+</style>
+    <!--[if lte IE 8]>
+		<script src="html5.js">
+    </script>
+		<![endif]-->
   </head>
   <body>
-    <div class="container-fluid">
-    <div id="header">
+
+    <div id="wrapper">
       <div id="logo">
         <img src="RSA logo.JPG"  alt="RSA logo"></div>
-      <div id="header-text">Radiology Support Unite</div>
-      <div id="right-header-text"> Events and Education</div>
-      <div id="top-nav">
+        <div id="right-header-text"> Events and Education</div>
+      <header>
+      <h1>Radiology Support Unite</h1>
 
-        <a href="index.html">Home</a>
-        <a href="page2.html">Tips Blogs and Forums</a>
-        <div class="nav-btn">Menu</div>
-        <div class="nav-btn">About Us</div>
-        <div class="nav-btn">Log In</div>
-        <div class="nav-btn">Search</div>
+      </header>
+      <div id="content">
+          <nav>
+          <ul>
+            <li class="btn"><a href="index.html">Home Page</a></li>
+             <li class="btn"><a href="page2.html">Tips Blogs and Forums</a></li>
+            <li class="btn"><a href="page4.html">Student</a></li>
+            <li class="btn">Search</li>
+            <li class="btn">Contact Us</li>
+            </ul>
+        </nav>
       </div>
     </div>
-    <div id="main-container">
+    <br class="clear">
 
-      <div id="left-column"><h3> Professional Organizations</h3>
+  <h3> Professional Organizations</h3>
 
         <?php
             $servername = "localhost";
@@ -71,7 +73,9 @@
 
 
 
-      <div id="main-content"> The main content will have links for different Radiology Support Events and Links for getting education for Radiology Support as well as continuing education for those already registered and or certified in Radiology Support.</div>
+<p>The main content will have links for different Radiology Support Events and Links
+for getting education for Radiology Support as well as continuing education for those already registered
+and or certified in Radiology Support.</p>
 
 
       <div id="content-text">
@@ -80,21 +84,25 @@
 
   </div>
 
-      <div id="mid-column">
+      <div id="midcol-right">
+        <figure>
+          <img src="Radiology.jpg" alt="Radiology">
+        </figure>
       </div>
 
 
     </div>
 
-    <div id="footer"></div>
-    <div id="footer=text"></div>
-    <div id="bottom-nav">
-      <div class="nav-btn">Back to Top</div>
-      <div class="nav-btn">Events and Education</div>
-      <div class="nav-btn">Copyright</div>
-      <div class="nav-btn">Privacy</div>
-      <div class="nav-btn">Legal</div>
-    </div>
+    <br class="clear">
+
+  <div id="footer"></div>
+  <div id="footer=text"></div>
+<div id="bottom-nav">
+  <div class="nav-btn"><a href="index.html">Home Page</a></div>
+  <div class="nav-btn">Copyright</div>
+  <div class="nav-btn">Privacy</div>
+  <div class="nav-btn">Legal</div>
+        </div>
 
 </div>
   </body>
